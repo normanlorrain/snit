14dbb25db4917a8faac96e0ee974108b16ba7c68
Norman Lorrain <normanlorrain@gmail.com>
2021-06-09 15:16:09 -0600

    Enabled Black.  Initial work on backup, with version numbers.

--- snit/archive.py
+++ snit/archive.py
@@ -1,13 +1,34 @@
-import os
+from pathlib import Path
+import shutil
 
-snit_dir = None
+_dir: Path = None
 
 
-def backup():
+def createFileNameFromPath(path: Path):
+    # for old,new in [(r'\\','_')]
+    path = path.as_posix()
+    path = path.replace("/", "_")
+    path = path.replace(":", "-")
+    return path
+
+
+def backup(path: Path):
+    if not _dir.exists():
+        _dir.mkdir(parents=True, exist_ok=True)
+
+    for child in path.iterdir():
+        version = 1
+        backups = list(_dir.glob("*"))
+        # if child.name in _dir.glob()
+        suffix = f".{version}{child.suffix}"
+        backupFile = _dir / (child.with_suffix(suffix).name)
+        shutil.copyfile(child, backupFile)
     pass
+
 
 def restore():
     pass
+
 
 def compare():
     pass
--- snit/cli.py
+++ snit/cli.py
@@ -1,17 +1,29 @@
+from pathlib import Path
 import click
 from . import archive
 
 
 @click.group()
-#@click.option('--debug/--no-debug', default=False)
-@click.option('--path', envvar='SNIT_DIR',  )
-def cli(path):
-    #click.echo(f"Debug mode is {'on' if debug else 'off'}")
-    click.echo(f"SNIT directory is set to {path}")
-    archive.snit_dir = path
+# @click.option('--debug/--no-debug', default=False)
+@click.option(
+    "--directory",
+    envvar="SNIT_DIR",
+)
+def cli(directory):
+    # click.echo(f"Debug mode is {'on' if debug else 'off'}")
+    click.echo(f"SNIT directory is set to {directory}")
+    snit_dir = Path(directory)
+    if not snit_dir.exists():
+        raise Exception(
+            "Must specify directory, either on command line or environment variable"
+        )
+
+    archive._dir = snit_dir / archive.createFileNameFromPath(Path.cwd())
+
+    pass
 
 
 @cli.command()  # @cli, not @click!
 def backup():
     click.echo("backup")
-    archive.backup()
+    archive.backup(Path.cwd() / Path(".vscode"))
