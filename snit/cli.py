3c7a307dd721c35c6bf52462fc57db62ac72e2ad
Norman Lorrain <normanlorrain@gmail.com>
2021-06-14 19:35:25 -0600

    help and error checking.

--- snit/cli.py
+++ snit/cli.py
@@ -6,33 +6,39 @@ from . import archive
 @click.group()
 # @click.option('--debug/--no-debug', default=False)
 @click.option(
-    "--directory",
+    "--archive",
+    "-a",
+    "directory",
     envvar="SNIT_DIR",
-    help="directory to store settings.  Can be set with the SNIT_DIR environment variable.",
+    metavar="PATH",
+    required=True,
+    help="Specify the directory for the archive.  Can be set with the SNIT_DIR environment variable.",
 )
 def cli(directory):
-    # click.echo(f"Debug mode is {'on' if debug else 'off'}")
-    click.echo(f"SNIT directory is set to {directory}")
+    """A utility to backup IDE/editor settings.
+
+
+    Currently only supports VSCode.
+    """
     snit_dir = Path(directory)
-    if not snit_dir.exists():
-        raise Exception(
-            "Must specify directory, either on command line or environment variable"
-        )
+    if snit_dir.exists():
+        click.echo(f"SNIT directory is set to {directory}")
+    else:
+        click.echo(f"SNIT directory {directory} not found", err=True)
+        raise click.Abort()
 
     archive._archive_dir = (
         snit_dir / archive.createBackupFolderName(Path.cwd()) / Path("vscode")
     )
 
-    pass
 
-
-@cli.command()  # @cli, not @click!
+@cli.command(help="Backup editor settings.")  # @cli, not @click!
 def backup():
     click.echo("backup")
     archive.backup(Path.cwd() / Path(".vscode"))
 
 
-@cli.command()  # @cli, not @click!
+@cli.command(help="List any found backups.")  # @cli, not @click!
 def list():
     click.echo("list")
     archive.list(Path.cwd() / Path(".vscode"))
